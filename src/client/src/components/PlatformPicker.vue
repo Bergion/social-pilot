04c06fcbe8b568e4834f45975470cc299184abc2
Dmitriy Vintsyk <dmitriy.vintsyk@devoxsoftware.com>
2024-05-29 15:05:34 +0300

    Add platform picker

---
+++ src/client/src/components/PlatformPicker.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+import TikTokIcon from '@/components/TikTokIcon.vue'
+
+const selection = ref([])
+</script>
+
+<template>
+	<v-item-group
+		v-model="selection" 
+		multiple 
+		class="mt-8"
+	>
+		<v-container>
+			<v-row>
+				<p class="font-bold mb-4">Select platform</p>
+			</v-row>
+			<v-row>
+					<v-item v-slot="{ isSelected, toggle }">
+						<v-btn
+							:color="isSelected ? 'green' : ''"
+							class="d-flex align-center w-12"
+							variant="outlined"
+							@click="toggle"
+						>
+							<v-icon size="x-large">
+								mdi-instagram
+							</v-icon>
+						</v-btn>
+					</v-item>
+					<v-item v-slot="{ isSelected, toggle }">
+						<v-btn
+							variant="outlined"
+							:color="isSelected ? 'green' : ''"
+							class="d-flex align-center ml-4"
+							dark
+							@click="toggle"
+						>
+              <TikTokIcon class="w-6"></TikTokIcon>
+            </v-btn>
+					</v-item>
+			</v-row>
+		</v-container>
+	</v-item-group>
+</template>
--- src/client/src/views/PostCreationView.vue
+++ src/client/src/views/PostCreationView.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
 import FileUploader from '@/components/FileUploader.vue'
+import PlatformPicker from '@/components/PlatformPicker.vue' 
 </script>
 
 <template>
   <v-container class="bg-white w-full" fluid>
 		<v-row>
-			<v-col cols="12" md="6">
+			<v-col cols="12" sm="6">
         <p class="text-3xl font-extrabold">Create new post</p>
+				<PlatformPicker></PlatformPicker>
+				<FileUploader class="mt-12"></FileUploader>
       </v-col>
-		</v-row>
-		<v-row>
-			<v-col cols="12" md="6">
-				<FileUploader></FileUploader>
+			<v-col md="6 flex justify-center">
+				<v-date-picker></v-date-picker>
 			</v-col>
 		</v-row>
 	</v-container>
 </template>
+<style>
+.v-picker-title {
+	display: none;
+}
+
+</style>
